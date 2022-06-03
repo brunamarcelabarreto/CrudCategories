223972efe58317c5ca1a322ee97b0f98632ac769
brunamarcelabarreto <bruna_marcelabarreto@hotmail.com>
2022-06-02 23:58:28 -0300

    Added Cors

--- dist/index.js
+++ dist/index.js
@@ -5,11 +5,18 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+const cors = require('cors');
+app.use(cors());
+app.get('/products/:id', function (req, res, next) {
+    res.json({ msg: 'This is CORS-enabled for all origins!' });
+});
+app.listen(4000, function () {
+    console.log('CORS-enabled web server listening on port 4000');
+});
 app.use(express_1.default.json());
 app.use(require('./routes/index'));
 app.get("/category", (req, res) => {
     console.log("aqui");
     return res.json();
 });
-app.listen(4000, () => console.log("Up!"));
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,18 @@ import express from 'express';
 
 const app = express();
 
+const cors = require('cors')
+
+app.use(cors())
+
+app.get('/products/:id', function (req: any, res: { json: (arg: { msg: string; }) => void; }, next: any) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+})
+
+app.listen(4000, function () {
+  console.log('CORS-enabled web server listening on port 4000')
+})
+
 app.use(express.json());
 app.use(require('./routes/index'));
 app.get("/category", (req, res) => {
@@ -9,6 +21,6 @@ app.get("/category", (req, res) => {
   return res.json();
 });
 
-app.listen(4000, () => console.log("Up!"));
+
 
 
